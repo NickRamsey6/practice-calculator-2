3ecf9888bb2e2843e44dbbcbc60b81b4e37fa892
Nick Ramsey <nramseysc@gmail.com>
2019-02-20 18:48:19 -0800

    updated front-end logic to allow users to use one button for all four functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,34 @@
 $(document).ready(function() {
+// Business Logic
+  var multiply = function(number1, number2){
+  return number1 * number2;
+  };
+  var add = function(number1, number2){
+  return number1 + number2;
+  };
+  var subtract = function(number1,number2){
+  return number1 - number2;
+  };
+  var divide = function(number1,number2){
+  return number1 / number2;
+  };
 
-var multiply = function(number1, number2){
-return number1 * number2;
-};
-var add = function(number1, number2){
-return number1 + number2;
-};
-var subtract = function(number1,number2){
-return number1 - number2;
-};
-var divide = function(number1,number2){
-return number1 / number2;
-};
-  $("#add").click(function(event) {
-
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("#subtract").click(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-
-  });
-  $("#multiply").click(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-
-  });
-  $("#divide").click(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-
+// User Interface or Front-End Logic
+$("form#calculator").submit(function() {
+  event.preventDefault();
+  var number1 = parseInt($("#input1").val());
+  var number2 = parseInt($("#input2").val());
+  var operator = $("input:radio[name=operator]:checked").val();
+  var result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+  $("#output").text(result);
   });
 });
